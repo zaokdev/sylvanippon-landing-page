979fe7dbf136ca2f9a07b45fae65f60b4cf28cda
Kevin Zapata <148372120+KevinZA0K@users.noreply.github.com>
2024-09-22 23:38:00 -0600

    Agregado hero section

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'hero-image': "url(/bg.jpg)"
+			},
+			fontFamily: {
+				mono: ['Bungee', ...defaultTheme.fontFamily.mono]
+			},
+			keyframes: {
+				heroimage: {
+					'0%': {transform: "translateX(200%)"},
+					'100%': {transform: "translateX(0)"}
+				},
+				herotext: {
+					'0%': {transform: "translateX(-100%)"},
+					'100%': {transform: "translateX(0)"}
+				},
+				bgfadein: {
+					'0%': {opacity: "0"},
+					'100%': {opacity: "1"}
+				}
+			}
+		},
+	},
+	plugins: [],
+}
